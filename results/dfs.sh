e1cc63dbccdbfe0686c10b0982bb1b9dbeac51da
MatheusF <matheussanches531@gmail.com>
2020-04-15 15:05:45 -0300

    script para exec dfs

---
+++ results/dfs.sh
@@ -0,0 +1,10 @@
+for ((n=1;n<10;n++))
+do
+	echo $n
+	python ../main.py --dfs --map="../maps/map$n.txt" | grep -e Time -e Node | sed 's/^......//' > dfs_map$n.out
+	for ((i=0;i<32;i++))
+	do
+		python ../main.py --dfs --map="../maps/map$n.txt" | grep -e Time -e Node | sed 's/^......//' >> dfs_map$n.out
+	done 
+	python bfs.py --file="./dfs_map$n.out"
+done
