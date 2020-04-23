e528e9bc37cedad4c79d710c12d3467a1e48221c
lladeira <lladeira@cpqd.com.br>
2020-04-23 15:24:35 -0300

    Bug fix, ty @MatheusFerraroni

--- execute.sh
+++ execute.sh
@@ -7,12 +7,12 @@ do
   do
   	echo $n
 
-  	python3 main.py --$value --print --map="/maps/map$n.txt" | grep -e Time -e Node | sed 's/^......//' > ./results/${value}_map$n.out
+  	python3 main.py --$value --print --map="./maps/map$n.txt" | grep -e Time -e Node | sed 's/^......//' > ./results/${value}_map$n.out
   	for ((i=0;i<32;i++))
   	do
-  		python3 main.py --$value --print --map="/maps/map$n.txt" | grep -e Time -e Node | sed 's/^......//' >> ./results/${value}_map$n.out
+  		python3 main.py --$value --print --map="./maps/map$n.txt" | grep -e Time -e Node | sed 's/^......//' >> ./results/${value}_map$n.out
   	done
-  	python3 ./results/compute.py --file="/results/${value}_map$n.out"
+  	python3 ./results/compute.py --file="./results/${value}_map$n.out"
 
   done
 
