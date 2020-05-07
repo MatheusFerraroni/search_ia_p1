56c353fbdd01f3eb75fcdc70030e5fa9756caf6b
oscarciceri <oscarcicericoral@gmail.com>
2020-05-07 15:39:50 -0500

    Execute ready to simulate

--- execute.sh
+++ execute.sh
@@ -42,25 +42,25 @@ do
 
 done
 
-########################################## NO POINTS ########################################################
+######################################### NO POINTS ########################################################
 
-# for value in lbs dfs bfs aos ats 
-# do
+for value in lbs dfs bfs aos ats 
+do
 
-#   echo ${value}
+  echo ${value}
 
-#   for ((n=1;n<13;n++))
-#   do
-#     echo $value " MAP3-Scenario = " $n
+  for ((n=1;n<13;n++))
+  do
+    echo $value " MAP3-Scenario = " $n
 
-#     python main.py --$value --print --map="./maps2/no_points/map$n.txt" | grep -e Time -e Node  -e Pont -e Left -e Acti | sed 's/^......//' > ./results/no_points/${value}_map$n.out
-#     for ((i=0;i<10;i++))
-#     do
-#       echo "Seed = " $i
-#       python main.py --$value --print --map="./maps2/no_points/map$n.txt" | grep -e Time -e Node   -e Pont -e Left -e Acti | sed 's/^......//' >> ./results/no_points/${value}_map$n.out
-#     done
-#     python ./results/compute.py --file="./results/no_points/${value}_map$n.out"
+    python main.py --$value --print --map="./maps2/no_points/map$n.txt" | grep -e Time -e Node  -e Pont -e Left -e Acti -e Expa | sed 's/^......//' > ./results/no_points/${value}_map$n.out
+    for ((i=0;i<10;i++))
+    do
+      echo "Seed = " $i
+      python main.py --$value --print --map="./maps2/no_points/map$n.txt" | grep -e Time -e Node   -e Pont -e Left -e Acti -e Expa | sed 's/^......//' >> ./results/no_points/${value}_map$n.out
+    done
+    python ./results/compute.py --file="./results/no_points/${value}_map$n.out"
 
-#   done
+  done
 
-# done
+done
